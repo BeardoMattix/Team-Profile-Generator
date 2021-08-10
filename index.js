81191c70bdfad1a694c8a1ab0b41d3150b7c5753
Beardo Mattix <christopher.mattix@gmail.com>
2021-08-10 09:36:40 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+class Employee {
+    constructor(name, id, email, officeNumber)
+}
+
+
+
+/* Employee Object Requirements 
+Employee Types: 
+* Manager
+* Engineer
+* Intern
+
+Manager/Employee Information:
+Name, ID, Email, Office Number
+
+
