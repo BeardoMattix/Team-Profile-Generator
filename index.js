4c6e54a963ca018f9b5968b13b041469406639c6
Beardo Mattix <christopher.mattix@gmail.com>
2021-08-11 11:56:42 -0400

    updating index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
-const generateHTML = require("./src/generateHTML")
+// const generateHTML = require("./src/generateHTML")
 const {
     employeeQuestions,
     managerQuestions,
@@ -13,3 +13,52 @@ const {
     internQuestions,
     addNewMember,
 } = require("./src/questions");
+
+const managerArray = [];
+const engineerArray = [];
+const internArray = [];
+
+
+const createTeam = async () => {
+    let employee = await inquirer.prompt(employeeQuestions);
+    try {
+        switch (employee.role) {
+            case "Manager":
+                let officeObj = await inquirer.prompt(managerQuestions);
+                const manager = new Manager(
+                    employee.name,
+                    employee.id,
+                    employee.email,
+                    employee.officeNumber
+                    );
+                    managerArray.push(manager);
+                    break;
+
+            case "Engineer":
+                let gitHubObj = await inquirer.prompt(engineerQuestions);
+                const engineer = new Engineer (
+                    employee.name,
+                    employee.id,
+                    employee.email,
+                    employee.github,
+                );
+                engineerArray.push(engineer);
+                break;
+
+            case "Intern":
+                let schoolObj = await inquirer.prompt(internQuestions);
+                const intern = new Intern(
+                    employee.name,
+                    employee.id,
+                    employee.email,
+                    employee.school,
+                );
+                internArray.push(intern);
+                break;
+                default: "";
+        } 
+    }
+    catch (error){
+        console.log(error);
+    }
+};
